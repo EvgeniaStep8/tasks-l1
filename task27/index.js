619d45a79f56be0719190ad14fbfd8814b432496
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-08-30 14:05:00 +0300

    add comments in task27

--- task27/index.js
+++ task27/index.js
@@ -1,9 +1,13 @@
 // Добавить анимацию для элемента: Напишите функцию, которая добавляет анимацию для элемента на веб-странице, например, плавное изменение его положения или размера.
 
-const element = document.querySelector(".element");
+// Находим паравоз, которому будем добавлять анимацию
+const train = document.querySelector(".train");
 
+// Функция добавления элементу анимацию, которая принимает на вход элемент, который нужно проанимировать
 const addAnimationToDomElement = (element) => {
+  // Задаём начальное смещение 0
   let shift = 0;
+  // Через setInterval будем каждые 16мс вызывать функцию, которая будет измещать смещение на 1 и добавлять элементу стиль margin-left равный смещению в px, пока смещение не достигнет 1000
   setInterval(() => {
     if (shift < 1000) {
       shift += 1;
@@ -12,4 +16,7 @@ const addAnimationToDomElement = (element) => {
   }, 16);
 };
 
-addAnimationToDomElement(element);
+// Навесим слушатель на элемент паравоза, который будет срабатывать при клике на паровоз и вызывать функцию для анимации элемента
+train.addEventListener("click", () => {
+  addAnimationToDomElement(train);
+})
