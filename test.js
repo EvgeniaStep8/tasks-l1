6d8a76ddc2fa0a8d5a030e7a881b4b9f8d026359
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-30 19:48:05 +0300

    add task 16 solution

--- test.js
+++ test.js
@@ -1,22 +1,7 @@
-const BASE_URL =
-  "http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true";
+const formatDate = require("./task16");
 
-let infoArray = [];
-
-const b = document.querySelector(".b");
-
-async function getData() {
-  try {
-    const result = await fetch(BASE_URL);
-    const data = await result.json();
-    infoArray = data;
-  } catch {
-    console.log("err");
-  }
-}
-
-getData();
-
-b.addEventListener("click", () => {
-  console.log(infoArray.reverse());
-})
+// В таком же формате будет текущая дата
+console.log(formatDate("MMMM Do YYYY, h:mm:ss a")); // August 30th 2023, 5:50:43 pm
+console.log(formatDate("dddd")); // Wednesday
+console.log(formatDate("MMM Do YY")); // Aug 30th 23
+console.log(formatDate()); // 2023-08-30T17:50:43+03:00
